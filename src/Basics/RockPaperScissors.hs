cfc4c25a5cb00280c339cd00409b7e5b7f2966a3
Christopher Mazey <cjmazey@gmail.com>
2015-03-24 16:25:43 -0400

    rock paper scissors

--- src/Basics/RockPaperScissors.hs
+++ src/Basics/RockPaperScissors.hs
@@ -1,1 +1,21 @@
 module Basics.RockPaperScissors where
+
+data Move
+  = Rock
+  | Paper
+  | Scissors
+  deriving (Eq,Read,Show,Enum,Bounded)
+
+data Outcome
+  = Lose
+  | Tie
+  | Win
+  deriving (Show,Eq,Ord)
+
+outcome :: Move -> Move -> Outcome
+outcome Rock Scissors = Win
+outcome Paper Rock = Win
+outcome Scissors Paper = Win
+outcome us them
+  | us == them = Tie
+  | otherwise = Lose
